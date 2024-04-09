5d35b30dc6eab3e9178a29f51512bd687374da40
kylenvu <kylenvu@gmail.com>
2024-04-08 22:07:20 -0700

    added servo

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -4,14 +4,30 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static frc.robot.Constants.*;
+
 public class Claw extends SubsystemBase {
+  private final Servo m_servo;
+  
   /** Creates a new Claw. */
-  public Claw() {}
+  public Claw() {
+    m_servo = new Servo(kClawPort);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public Command raiseCommand() {
+    return this.run(() -> m_servo.set(m_servo.get() + 0.01));
+  }
+
+  public Command lowerCommand() {
+    return this.run(() -> m_servo.set(m_servo.get() - 0.01));
   }
 }
--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -4,14 +4,30 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static frc.robot.Constants.*;
+
 public class Wrist extends SubsystemBase {
+  private final Servo m_servo;
+  
   /** Creates a new Wrist. */
-  public Wrist() {}
+  public Wrist() {
+    m_servo = new Servo(kWristPort);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public Command raiseCommand() {
+    return this.run(() -> m_servo.set(m_servo.get() + 0.01));
+  }
+
+  public Command lowerCommand() {
+    return this.run(() -> m_servo.set(m_servo.get() - 0.01));
   }
 }
