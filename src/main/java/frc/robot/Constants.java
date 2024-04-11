9bf21ea346176a3bcec52ec18092bffb7f91ba8b
ktvuwhs <ktvu@hbuhsd.edu>
2024-04-10 17:59:36 -0700

    refactored names

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,22 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final int kArmPort = 4;
-  public static final int kWristPort = 3;
-  public static final int kClawPort = 2;
+  public static final int kMotorRelaySignalPort = 8;
+
+  public final class LiftConstants {
+    // Max and min pulse values for Servo can be found https://www.pololu.com/docs/0J76/4
+    public static final int kPort = 4;
+    public static final double kMinPulse = 0; // 1000 microseconds -> 0
+    public static final double kMaxPulse = 900.0/1000.0;
+  }
+
+  public final class GripperConstants {
+    public static final int kPort = 2;
+  }
+
+  public final class PivotConstants {
+    public static final int kPort = 3;
+    public static final double kMinPulse = 200.0 / 1000.0; // 1200 microseconds out of 1000 is fully closed
+    public static final double kMaxPulse = 900.0 / 1000.0; // 1900 microseconds out of 2000 is fully open
+  }
 }
